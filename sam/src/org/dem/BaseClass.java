050a8d30b00c99e6758cde442bc540d3107deb3b
kamaraj12345 <chethrakamaraj9@gmail.com>
2022-01-07 22:21:31 +0530

    mes

---
+++ sam/src/org/dem/BaseClass.java
@@ -0,0 +1,30 @@
+package org.dem;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		
+
+		
+		for ( int i = 2; i < 5; ++i) {
+				
+			
+			for (int j = 1; j < i; j++) {
+				
+				System.out.print(j );
+				
+				
+			}
+			System.out.println();
+
+
+			
+			
+		}
+		
+		
+	}
+	
+	
+	
+}
---
+++ sam/src/org/dem/add.java
@@ -0,0 +1,45 @@
+package org.dem;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class add {
+
+	
+	
+	
+	@BeforeClass
+	public  void beforeClass() {
+		
+		
+		
+		
+		
+		}
+	
+	@AfterClass
+	public void afterClass() {
+
+	}
+	@BeforeTest
+	public void before() {
+		// TODO Auto-generated method stub
+
+	}@AfterTest
+	public void after() {
+		// TODO Auto-generated method stub
+
+
+}
+	@Test
+	public void test() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+}
+
